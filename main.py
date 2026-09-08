5c616fd231cf1a0c26d877cd2987526b01819886
bUDA <293315128+TheZenBuda@users.noreply.github.com>
2026-09-08 12:21:51 +0200

    Initial BookBot: read Frankenstein and count words

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text):
+    return len(text.split())
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+
+main()
